81e0e9ba5e3d9db788b3dd0128d57ecdc6bd35f7
sehyoun <lyutvs@gmail.com>
2021-09-01 13:49:06 +0900

    ⚡️ :: ChatNotifucation

--- chat-service/src/main/java/com/example/chatservice/model/ChatNotification.java
+++ chat-service/src/main/java/com/example/chatservice/model/ChatNotification.java
@@ -1,2 +1,17 @@
-package com.example.chatservice.model;public class ChatNotification {
+package com.example.chatservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatNotification {
+
+    private String id;
+    private String sendId;
+    private String senderName;
 }
