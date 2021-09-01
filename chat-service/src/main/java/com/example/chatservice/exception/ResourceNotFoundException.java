7accf17f88fbb2d648b1367f08b34a1b055d174d
sehyoun <lyutvs@gmail.com>
2021-09-01 13:49:59 +0900

    ⚡️ :: ResourceNotFoundException

--- chat-service/src/main/java/com/example/chatservice/exception/ResourceNotFoundException.java
+++ chat-service/src/main/java/com/example/chatservice/exception/ResourceNotFoundException.java
@@ -1,2 +1,11 @@
-package com.example.chatservice.exception;public class ResourceNotFoundException {
+package com.example.chatservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
