4c0cef4df7700594bfd4f1e7c9b1b87757f449b1
sehyoun <lyutvs@gmail.com>
2021-09-01 15:17:21 +0900

    ⚡️ :: Role

--- auth-service/src/main/java/com/example/springwebsocket/model/Role.java
+++ auth-service/src/main/java/com/example/springwebsocket/model/Role.java
@@ -1,8 +1,16 @@
 package com.example.springwebsocket.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    public final static Role USER = new Role("USER");
+    public final static Role FACEBOOK_USER = new Role("FACEBOOK_USER");
+
+    private String name;
 }
