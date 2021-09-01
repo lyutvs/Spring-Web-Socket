9e1d763283377781721cbef7cb8258a85a3de69e
sehyoun <lyutvs@gmail.com>
2021-09-01 13:48:07 +0900

    ⚡️ :: ChatMessageRepository

--- chat-service/src/main/java/com/example/chatservice/repository/ChatMessageRepository.java
+++ chat-service/src/main/java/com/example/chatservice/repository/ChatMessageRepository.java
@@ -1,2 +1,16 @@
-package com.example.chatservice.repository;public interface ChatMessageRepository {
+package com.example.chatservice.repository;
+
+import com.example.chatservice.model.ChatMessage;
+import com.example.chatservice.model.MessageStatus;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepository
+        extends MongoRepository<ChatMessage, String> {
+
+    long countBySenderIdAndRecipientIdAndStatus(
+            String senderId, String recipientId, MessageStatus status);
+
+    List<ChatMessage> findByChatId(String chatId);
 }
