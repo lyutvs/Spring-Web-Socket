2293546040d297bd0db77d3905d5253d928bfb9d
sehyoun <lyutvs@gmail.com>
2021-09-01 13:49:42 +0900

    ⚡️ :: ChatRoomService

--- chat-service/src/main/java/com/example/chatservice/service/ChatRoomService.java
+++ chat-service/src/main/java/com/example/chatservice/service/ChatRoomService.java
@@ -1,2 +1,47 @@
-package com.example.chatservice.service;public class ChatRoomService {
+package com.example.chatservice.service;
+
+import com.example.chatservice.model.ChatRoom;
+import com.example.chatservice.repository.ChatRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChatRoomService {
+
+    @Autowired private ChatRoomRepository chatRoomRepository;
+
+    public Optional<String> getChatId(
+            String senderId, String recipientId, boolean createIfNotExist) {
+
+        return chatRoomRepository
+                .findBySenderIdAndRecipientId(senderId, recipientId)
+                .map(ChatRoom::getChatId)
+                .or(() -> {
+                    if(!createIfNotExist) {
+                        return  Optional.empty();
+                    }
+                    var chatId =
+                            String.format("%s_%s", senderId, recipientId);
+
+                    ChatRoom senderRecipient = ChatRoom
+                            .builder()
+                            .chatId(chatId)
+                            .senderId(senderId)
+                            .recipientId(recipientId)
+                            .build();
+
+                    ChatRoom recipientSender = ChatRoom
+                            .builder()
+                            .chatId(chatId)
+                            .senderId(recipientId)
+                            .recipientId(senderId)
+                            .build();
+                    chatRoomRepository.save(senderRecipient);
+                    chatRoomRepository.save(recipientSender);
+
+                    return Optional.of(chatId);
+                });
+    }
 }
